480950dc7079af0bcc2e796729536f8e0bbfafe9
Caleb <calebh101dev@icloud.com>
2025-08-23 19:57:11 -0500

    Windows build script is now working, also add code of conduct in Linux build script

--- linux/build.sh
+++ linux/build.sh
@@ -26,6 +26,7 @@ mkdir -p x64
 cp $SCRIPT_DIR/build/Desktop_Qt_6_9_1-Release/AboutThisPC x64/AboutThisPC
 cp $PARENT_DIR/README.md x64/README.md
 cp $PARENT_DIR/LICENSE.md x64/LICENSE.md
+cp $PARENT_DIR/SECURITY.md x64/SECURITY.md
 cp $PARENT_DIR/CONTRIBUTING.md x64/CONTRIBUTING.md
 sed -e "s/\[\[APPVERSION\]\]/$VERSION/g" -e "s/\[\[AUTHOR\]\]/$AUTHOR/g" "$SCRIPT_DIR/Sample.desktop" > x64/AboutThisPC.desktop
 
--- windows/build.ps1
+++ windows/build.ps1
@@ -5,69 +5,44 @@ $WindowsDir = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
 $ParentDir = Split-Path -Path $WindowsDir -Parent
 $OutputDir = "$ParentDir\Output\windows"
 
+if (Test-Path $OutputDir) {
+    Remove-Item $OutputDir -Recurse -Force
+}
+
 Write-Output "Creating About This PC version $version..."
 Set-Location -Path "$WindowsDir\AboutThisPC"
-
-function GetSed {
-    param($Target)
-    $TargetDir="$OutputDir\$Target-Exe"
-
-    $content = @"
-[Version]
-Class=IEXPRESS
-SEDVersion=3
-
-[Options]
-PackagePurpose=InstallApp
-ShowInstallProgramWindow=1
-HideExtractAnimation=0
-UseLongFileName=1
-OverwriteInstallFiles=2
-TargetName=${TargetDir}\AboutThisPC-${Target}.exe
-FriendlyName=AboutThisPC ${Target}
-CreateCAB=0
-InstallPrompt=%NoPrompt%
-DisplayLicense=%No%
-FinishMessage=%No%
-
-[SourceFiles]
-SourceFiles0=${OutputDir}\${Target}-Raw
-
-[SourceFiles0]
-*.*=*
-
-[DestinationDirs]
-DefaultDir=C:\Temp\Extracted
-
-[InstallExecute]
-AboutThisPC.exe
-"@
-
-    $path = "C:\Temp\AboutThisPC.sed"
-    $directory = Split-Path $path -Parent
-
-    if (-not (Test-Path $directory)) {
-        New-Item -ItemType Directory -Path $directory -Force
-    }
-
-    if (-not (Test-Path $TargetDir)) {
-        New-Item -ItemType Directory -Path $TargetDir -Force
-    }
-
-    $content | Out-File -FilePath $path -Encoding ASCII
-    return $path;
-}
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
 function Build {
     param($Target)
-    $sedPath=GetSed -Target "$Target"
+    $directory="$OutputDir\$Target-Raw"
 
     Write-Output "Building application for $Target..."
-    #dotnet publish -c Release -r "$Target" --self-contained true -o "$OutputDir\$Target-Raw"
+    dotnet publish -c Release -r "$Target" --self-contained true -o "$directory"
 
-    Write-Output "Creating executable for path $sedPath..."
-    & "iexpress.exe" /N /Q /M "$sedPath"
+    Write-Output "Creating archive for $directory..."
+    Compress-Archive -Path "$directory\*" -DestinationPath "$OutputDir\$Target-Raw-Archive"
+
+    $directory="$OutputDir\$Target-Results"
+    Write-Output "Creating result for $directory..."
+
+    if (-not (Test-Path $directory)) {
+        New-Item -Path $directory -ItemType Directory
+    }
+
+    Copy-Item -Path "$OutputDir\$Target-Raw-Archive.zip" -Destination "$directory\AboutThisPC-Package.zip"
+    Copy-Item -Path "$ParentDir\README.md" -Destination "$directory\README.md"
+    Copy-Item -Path "$ParentDir\LICENSE.md" -Destination "$directory\LICENSE.md"
+    Copy-Item -Path "$ParentDir\SECURITY.md" -Destination "$directory\SECURITY.md"
+    Copy-Item -Path "$ParentDir\CONTRIBUTING.md" -Destination "$directory\CONTRIBUTING.md"
+    Copy-Item -Path "$ParentDir\CODE_OF_CONDUCT.md" -Destination "$directory\CODE_OF_CONDUCT.md"
+
+    Write-Output "Creating archive for $directory..."
+    Compress-Archive -Path "$directory\*" -DestinationPath "$ParentDir\Output\AboutThisPC-$version-$Target.zip"
 }
 
 Build -Target "win-x64"
 Build -Target "win-arm64"
+
+$stopwatch.Stop()
+Write-Output "All jobs done after $($stopwatch.Elapsed)! Version: $version"
